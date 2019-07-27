ebe746d364e3b503e215e0a539f8fa372d50c887
Ferdinand Prantl <prantlf@gmail.com>
2019-07-27 06:41:53 +0200

    fix: Correct the Baltic plural rules 1 and 2

--- src/cardinals.js
+++ src/cardinals.js
@@ -108,7 +108,7 @@ const cardinals = {
     // 32, 33, 34, 35, 36, 37, 38, 39, 42, 43, 44, 45, 46, 47, 48, 49, 52, 53,
     // 54, 55, 56, 57, 58, 59, 62, 63, 64, 65, 66, 67, 68, 69, 72, 73, ...
     n => n % 10 === 1 && n % 100 !== 11 ? 0
-      : n % 10 >= 2 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2,
+      : n % 10 >= 2 && (n % 100 < 10 || n % 100 >= 20) ? 2 : 1,
     // Plural rule #7 (3 forms)
     // Families: Slavic (Belarusian, Bosnian, Croatian, Serbian, Russian, Ukrainian)
     // Locales: be (Belarusian), bs (Bosnian), hr (Croatian), ru (Russian),
